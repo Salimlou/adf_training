821758dab421abf3db8ed12ba44850915c3bd5fc
Jack de Vil <geminica.programs@gmail.com>
2013-03-03 01:29:15 +0100

    initial ADFFirstApp commit

---
+++ ADFFirstApp/sqlpl.bat
@@ -0,0 +1,7 @@
+@echo off
+rem set NLS_LANG=DANISH_DENMARK.WE8MSWIN1252
+rem set NLS_LANG=DANISH_AMERICA.WE8ISO8859P15
+rem set NLS_LANG=DANISH_DENMARK.AL32UTF8
+set NLS_LANG=AMERICAN_AMERICA.AL32UTF8
+set ORACLE_HOME=h:\apps\Oracle\oraclexe\app\oracle\product\11.2.0\server
+sqlplus.exe %1
